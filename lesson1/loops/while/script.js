b0304e132dfdeb5b493f5de23065227c98ff0539
Your Name <you@example.com>
2023-06-07 18:16:20 +0300

    added

--- lesson1/loops/while/script.js
+++ lesson1/loops/while/script.js
@@ -1,1 +1,36 @@
 const grades = [88, 96, 75, 100, 101, 95, 34, 45, 85];
+
+const sum = () => {
+    let i = 0;
+    let total = 0;
+
+    while (i < grades.length) {
+        total += grades[i];
+
+        i++;
+    }
+
+    document.querySelector("#output1").innerHTML = total;
+}
+
+const showAsterisks = () => {
+    let i = 0;
+    const elem = document.querySelector("#output2");
+    elem.innerHTML = "";
+
+    while (i < 10) {
+        elem.innerHTML += "*<br>";
+
+        i++;
+    }
+}
+
+const showNumbers = () => {
+    let i = 1;
+    const elem = document.querySelector("#output3");
+    elem.innerHTML = "";
+
+    while (i <= 15) {
+        elem.innerHTML += `${i++}<br>`;
+    }
+}
