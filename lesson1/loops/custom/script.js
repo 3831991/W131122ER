454a6c485ab01ba2995c620427deed39f10b08b3
Your Name <you@example.com>
2023-06-07 21:01:41 +0300

    added

--- lesson1/loops/custom/script.js
+++ lesson1/loops/custom/script.js
@@ -78,5 +78,14 @@ const showSpecialAsterisks10 = () => {
 }
 
 const multiBoard = () => {
-    
+    const elem = document.querySelector("#board");
+    elem.innerHTML = "";
+
+    for (let y = 1; y <= 10; y++) {
+        for (let x = 1; x <= 10; x++) {
+            const div = document.createElement("div");
+            div.innerHTML = x * y;
+            elem.appendChild(div);
+        }
+    }
 }
