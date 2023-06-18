78a297ae4ea5c71c76a3eefcdb88937f4eef7f31
Your Name <you@example.com>
2023-06-18 18:49:45 +0300

    added

--- lesson4/memory/script.js
+++ lesson4/memory/script.js
@@ -8,11 +8,17 @@ for (let i = 1; i <= amount; i++) {
 }
 
 for (let i = 1; i <= amount * 2; i++) {
+    // אינדקס רנדומלי מהמערך
     const rand = Math.floor(Math.random() * numbers.length);
     
     const div = document.createElement("div");
-    div.innerHTML = numbers[rand];
+    div.innerHTML = `<span>${numbers[rand]}</span>`;
     board.appendChild(div);
 
+    // בכדי שלא יהיו כפילויות, לאחר שהשתמשנו במיקום מסויים, הסרנו אותו מהמערך
     numbers.splice(rand, 1);
+
+    div.addEventListener("click", ev => {
+        ev.target.classList.add("showing");
+    });
 }
