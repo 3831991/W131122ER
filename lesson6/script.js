86da2ebfdd49c75d953254cf54d93b0f45e37d08
Your Name <you@example.com>
2023-06-28 19:30:09 +0300

    added

--- lesson6/script.js
+++ lesson6/script.js
@@ -26,5 +26,5 @@ class Gallery {
     }
 }
 
-const g1 = new Gallery("gallery2", "./images/תמונה (1).jpg", "./images/תמונה (2).jpg", "./images/תמונה (3).jpg", "./images/תמונה (4).jpg", "./images/תמונה (5).jpg", "https://www.photo-art.co.il/wp-content/uploads/2018/09/J75_5568-1.jpg");
+const g1 = new Gallery("gallery1", "./images/תמונה (1).jpg", "./images/תמונה (2).jpg", "./images/תמונה (3).jpg", "./images/תמונה (4).jpg", "./images/תמונה (5).jpg", "https://www.photo-art.co.il/wp-content/uploads/2018/09/J75_5568-1.jpg");
 const g2 = new Gallery("gallery2", "./images/תמונה (11).jpg", "./images/תמונה (12).jpg", "./images/תמונה (13).jpg", "./images/תמונה (14).jpg", "./images/תמונה (15).jpg", "./images/תמונה (16).jpg", "./images/תמונה (17).jpg");
