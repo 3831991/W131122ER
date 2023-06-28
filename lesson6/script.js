9d0f8d4f69904a929b0c96072af9cc561878869c
Your Name <you@example.com>
2023-06-28 20:40:48 +0300

    added

--- lesson6/script.js
+++ lesson6/script.js
@@ -2,6 +2,7 @@ class Gallery {
     images = [];
     imgElem;
     currentImage = -1;
+    interval;
 
     constructor(elemId, ...imageUrls) {
         this.images = imageUrls;
@@ -12,28 +13,28 @@ class Gallery {
         // הוספת הלחצן הימני
         const right = document.createElement('div');
         right.classList.add('arrow', 'right');
+        right.addEventListener('click', () => this.prevImage());
         galleryElem.appendChild(right);
 
         // הוספת הלחצן השמאלי
         const left = document.createElement('div');
         left.classList.add('arrow', 'left');
+        left.addEventListener('click', () => this.nextImage());
         galleryElem.appendChild(left);
 
         // יצירת אלמנט של התמונה
         this.imgElem = document.createElement("img");
 
-        // הוספת אירועים לתמונות
-        this.imgElem.addEventListener('click', () => this.nextImage());
-        this.imgElem.addEventListener('contextmenu', ev => {
-            ev.preventDefault();
-            this.prevImage();
-        });
+        // הוספת אירועים לגלרייה
+        galleryElem.addEventListener('mouseover', () => this.stopAuto());
+        galleryElem.addEventListener('mouseout', () => this.startAuto());
 
         // הוספת התמונה לדף
         galleryElem.appendChild(this.imgElem);
 
         // לצורך הצגת התמונה הראשונה
         this.nextImage();
+        this.startAuto();
     }
 
     nextImage() {
@@ -55,7 +56,19 @@ class Gallery {
 
         this.imgElem.src = this.images[this.currentImage];
     }
+
+    startAuto() {
+        clearInterval(this.interval);
+
+        this.interval = setInterval(() => {
+            this.nextImage();
+        }, 3 * 1000);
+    }
+
+    stopAuto() {
+        clearInterval(this.interval);
+    }
 }
 
-const g1 = new Gallery("gallery1", "./images/תמונה (1).jpg", "./images/תמונה (2).jpg", "./images/תמונה (3).jpg", "./images/תמונה (4).jpg", "./images/תמונה (5).jpg", "https://www.photo-art.co.il/wp-content/uploads/2018/09/J75_5568-1.jpg");
+const g1 = new Gallery("gallery1", "./images/תמונה (1).jpg", "./images/תמונה (2).jpg", "./images/תמונה (3).jpg", "./images/תמונה (4).jpg", "./images/תמונה (5).jpg");
 const g2 = new Gallery("gallery2", "./images/תמונה (11).jpg", "./images/תמונה (12).jpg", "./images/תמונה (13).jpg", "./images/תמונה (14).jpg", "./images/תמונה (15).jpg", "./images/תמונה (16).jpg", "./images/תמונה (17).jpg");
