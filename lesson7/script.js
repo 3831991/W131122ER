f45bb5dc4149a74a248103770d26c68da33930c7
Your Name <you@example.com>
2023-07-02 20:04:36 +0300

    added

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,16 +1,40 @@
 function loginStatus() {
-    fetch("https://api.shipap.co.il/login")
-        .then(res => res.json())
-        .then(data => {
-            if (data.status == 'success') {
-                handelUserData(data.user);
-            } else {
-                handelUserData();
-            }
-        });
+    fetch("https://api.shipap.co.il/login", {
+        credentials: 'include', // מאפשר שליחה וקבלה של עוגיות
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.status == 'success') {
+            handelUserData(data.user);
+        } else {
+            handelUserData();
+        }
+    });
 }
 
+function login() {
+    const obj = {
+        userName: document.querySelector('#userName').value,
+        password: document.querySelector('input[type=password]').value,
+    };
 
+    fetch("https://api.shipap.co.il/login", {
+        method: 'POST',
+        credentials: 'include', // מאפשר שליחה וקבלה של עוגיות
+        headers: {
+            'Content-Type': 'application/json', // הגדרת סוג התוכן הנשלח לשרת
+        },
+        body: JSON.stringify(obj), // תוכן הקריאה לשרת
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.status == 'success') {
+            handelUserData(data.user);
+        } else {
+            handelUserData();
+        }
+    });
+}
 
 function handelUserData(user = null) {
     const divLogin = document.querySelector(".login");
