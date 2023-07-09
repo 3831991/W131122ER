d65d1dbb6e646e56c2a00573eeeed4203c5d8a94
Your Name <you@example.com>
2023-07-09 18:54:58 +0300

    added

---
+++ lesson9/hello-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>האתר הראשון בריאקט</h1>
+      
+      <div className="frame">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
