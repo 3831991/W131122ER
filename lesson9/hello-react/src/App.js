8ff18d0eef4d4624364dab4b2389504a6f674961
Your Name <you@example.com>
2023-07-12 18:28:20 +0300

    added

--- lesson9/hello-react/src/App.js
+++ lesson9/hello-react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Counter from './Counter';
+import Gallery from './Gallery';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>האתר הראשון בריאקט</h1>
 
       <div className="frame">
+        <Gallery />
         <Counter />
       </div>
     </div>
---
+++ lesson9/hello-react/src/Gallery.js
@@ -0,0 +1,26 @@
+import './Gallery.css';
+
+function Gallery() {
+    const images = [
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(1).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(2).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(3).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(4).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(5).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(6).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(7).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(8).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(9).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(10).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(11).jpg',
+        'https://3831991.github.io/W050323ER/lesson19/simple-gallery/images/%D7%AA%D7%9E%D7%95%D7%A0%D7%94%20(12).jpg',
+    ];
+
+    return (
+        <div className="gallery">
+            <img src={images[0]} />
+        </div>
+    );
+}
+
+export default Gallery;
