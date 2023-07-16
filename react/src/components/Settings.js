35b8088e5ae22cdb7eb7c82d8f36aabf4aa8dcc5
Your Name <you@example.com>
2023-07-16 20:11:15 +0300

    added

--- react/src/components/Settings.js
+++ react/src/components/Settings.js
@@ -4,12 +4,17 @@ import { useState } from "react";
 export default function Settings() {
     const [brightness, setBrightness] = useState(55);
 
+    function changeBrightness(ev) {
+        setBrightness(ev.target.value);
+        document.querySelector('html').style.filter = `brightness(${brightness}%)`;
+    }
+
     return (
         <div className="Settings">
             <div className="range">
                 <b>בהירות:</b>
-                <input type="range" min={10} max={100} value={brightness} onChange={ev => setBrightness(ev.target.value)} />
-                <input type="number" min={10} max={100} value={brightness} onChange={ev => setBrightness(ev.target.value)} />
+                <input type="range" min={10} max={100} value={brightness} onChange={changeBrightness} />
+                <input type="number" min={10} max={100} value={brightness} onChange={changeBrightness} />
             </div>
         </div>
     )
