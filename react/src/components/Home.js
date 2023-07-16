e0612694eee1385ec7cd383d12813f20fe261e1f
Your Name <you@example.com>
2023-07-16 18:31:06 +0300

    added

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -8,6 +8,10 @@ function Home() {
             <Numbers min={15} max={50} />
             <Numbers min={30} max={80} />
             <Numbers min={100} max={120} />
+
+            <Square width={400} height={300} bg="blue" />
+            <Square width={600} height={200} bg="green" />
+            <Square width={800} height={500} bg="yellow" />
         </>
     )
 }
