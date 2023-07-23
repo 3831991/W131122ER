97c437b10a78177f1da879d6d12723fb358c71bd
Your Name <you@example.com>
2023-07-23 20:34:05 +0300

    added

--- react/src/components/UsersCards.js
+++ react/src/components/UsersCards.js
@@ -1,10 +1,27 @@
 import { users } from "./UsersData";
-import { useState } from 'react';
+import moment from "moment";
 
 export default function UsersCards() {
-    const [data, setData] = useState(users);
+    users.forEach(u => {
+        u.age = moment().diff(u.birthday, 'years');
+    });
+
+    const maxAge = Math.max(...users.map(u => u.age));
+    const minAge = Math.min(...users.map(u => u.age));
 
     return (
-        <p>כרטיסים</p>
+        <div className="cardFrame">
+        {
+            users.map(u => 
+                <div className={'card' + (u.age === maxAge ? ' max-age' : '') + (u.age === minAge ? ' min-age' : '')} key={u.id}>
+                    <p><b>{u.firstName} {u.lastName}</b></p>
+                    <p><b>טלפון:</b> {u.phone}</p>
+                    <p><b>אימייל:</b> {u.email}</p>
+                    <p><b>תאריך לידה:</b> {moment(u.birthday).format('DD/MM/YYYY')}</p>
+                    <p><b>גיל:</b> {u.age}</p>
+                </div>    
+            )
+        }
+        </div>
     )
 }
