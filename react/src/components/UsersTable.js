2168eddbbf96050a8f0773f3ecfc9b442cd48797
Your Name <you@example.com>
2023-07-23 18:41:27 +0300

    added

--- react/src/components/UsersTable.js
+++ react/src/components/UsersTable.js
@@ -1,3 +1,4 @@
+import './Users.css';
 import { users } from "./UsersData";
 import { useState } from 'react';
 
@@ -5,6 +6,29 @@ export default function UsersTable() {
     const [data, setData] = useState(users);
 
     return (
-        <p>טבלה</p>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>שם מלא</th>
+                    <th>אימייל</th>
+                    <th>טלפון</th>
+                    <th>תאריך לידה</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                data.map((u, i) => 
+                    <tr>
+                        <td>{i + 1}</td>
+                        <td>{u.firstName} {u.lastName}</td>
+                        <td>{u.email}</td>
+                        <td>{u.phone}</td>
+                        <td>{u.birthday}</td>
+                    </tr>
+                )
+            }
+            </tbody>
+        </table>
     )
 }
