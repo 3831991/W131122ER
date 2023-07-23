ee9a3d02bc1f8c634dbe77dedd807c0eeaea60d9
Your Name <you@example.com>
2023-07-23 19:01:12 +0300

    added

--- react/src/components/UsersTable.js
+++ react/src/components/UsersTable.js
@@ -5,6 +5,12 @@ import { useState } from 'react';
 export default function UsersTable() {
     const [data, setData] = useState(users);
 
+    const removeUser = (userId) => {
+        const newData = data.filter(u => u.id !== userId);
+
+        setData(newData);
+    }
+
     return (
         <table>
             <thead>
@@ -14,17 +20,21 @@ export default function UsersTable() {
                     <th>אימייל</th>
                     <th>טלפון</th>
                     <th>תאריך לידה</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
             {
                 data.map((u, i) => 
-                    <tr>
+                    <tr key={u.id}>
                         <td>{i + 1}</td>
                         <td>{u.firstName} {u.lastName}</td>
                         <td>{u.email}</td>
                         <td>{u.phone}</td>
                         <td>{u.birthday}</td>
+                        <td>
+                            <button className="remove" onClick={() => removeUser(u.id)}>❌</button>
+                        </td>
                     </tr>
                 )
             }
