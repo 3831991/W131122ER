067e1086139d6e3b600f0581eb6080516b079acf
Your Name <you@example.com>
2023-07-30 19:10:50 +0300

    added

--- react/src/components/Contact.js
+++ react/src/components/Contact.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './Contact.css';
+import Joi from 'joi';
+import { JOI_HEBREW } from '../joi-hebrew';
 
 export default function Contact() {
     const [formData, setFormData] = useState({
@@ -7,6 +9,15 @@ export default function Contact() {
         phone: '',
         email: '',
         message: '',
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const contactSchema = Joi.object({
+        fullName: Joi.string().min(3).max(30).required(),
+        phone: Joi.string().min(9).max(12).required(),
+        email: Joi.string().min(3).required(),
+        message: Joi.string().min(20).max(500).required(),
     });
 
     const handelInput = ev => {
@@ -17,7 +28,20 @@ export default function Contact() {
             [name]: value,
         };
 
+        const schema = contactSchema.validate(obj, { abortEarly: false, messages: { he: JOI_HEBREW }, errors: { language: 'he' } });
+
+        const err = {
+            ...errors,
+            [name]: undefined,
+        };
+
+        if (schema.error) {
+            const e = schema.error.details.find(x => x.context.key === name);
+            err[name] = e?.message;
+        }
+
         setFormData(obj);
+        setErrors(err);
     }
 
     const send = ev => {
@@ -45,20 +69,28 @@ export default function Contact() {
                     <input type="text" name="fullName" value={formData.fullName} onChange={handelInput} />
                 </label>
 
+                {errors.fullName && <div className='fieldError'>{errors.fullName}</div>}
+
                 <label>
                     טלפון:
                     <input type="tel" name="phone" value={formData.phone} onChange={handelInput} />
                 </label>
+
+                {errors.phone && <div className='fieldError'>{errors.phone}</div>}
 
                 <label>
                     אימייל:
                     <input type="email" name="email" value={formData.email} onChange={handelInput} />
                 </label>
 
+                {errors.email && <div className='fieldError'>{errors.email}</div>}
+
                 <label>
                     הודעה:
                     <textarea name="message" value={formData.message} onChange={handelInput}></textarea>
                 </label>
+
+                {errors.message && <div className='fieldError'>{errors.message}</div>}
 
                 <button>שלח</button>
             </form>
