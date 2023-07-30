6d548e4d89813b9e50c8abcad7d3fa27e2e3f425
Your Name <you@example.com>
2023-07-30 21:03:02 +0300

    added

--- react/src/components/Tickets.js
+++ react/src/components/Tickets.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import moment from "moment";
 import './Tickets.css';
 
 export default function Tickets() {
@@ -14,7 +15,32 @@ export default function Tickets() {
 
     return (
         <div className="Tickets">
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>תאריך</th>
+                        <th>שם מלא</th>
+                        <th>טלפון</th>
+                        <th>אימייל</th>
+                        <th>הודעה</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    data.map((t, i) => 
+                        <tr>
+                            <td>{i + 1}</td>
+                            <td>{moment(t.createTime).format('D.M.YY')}</td>
+                            <td>{t.fullName}</td>
+                            <td>{t.phone}</td>
+                            <td>{t.email}</td>
+                            <td>{t.message}</td>
+                        </tr>
+                    )
+                }
+                </tbody>
+            </table>
         </div>
     )
 }
