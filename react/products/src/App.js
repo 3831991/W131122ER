42b086098ec8492656c7359d685ff9f728a04912
Your Name <you@example.com>
2023-08-02 18:51:58 +0300

    added

--- react/products/src/App.js
+++ react/products/src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Products from './products/Products';
+import Login from './user/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>ניהול מוצרים</h1>
+    const [user, setUser] = useState();
+    const [isLogged, setIsLogged] = useState();
 
-      <div className="frame">
+    useEffect(() => {
+        fetch("https://api.shipap.co.il/login", {
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status === 'success') {
+                setUser(data.user);
+                setIsLogged(true);
+            } else {
+                setUser();
+                setIsLogged(false);
+            }
+        });
+    }, []);
 
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>ניהול מוצרים</h1>
+
+            <div className="frame">
+                {
+                    isLogged ? <Products /> : <Login />
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
