5cb9dbe5f448ab52321f535fb5213bcd499a1f6e
Your Name <you@example.com>
2023-08-09 19:41:11 +0300

    added

--- react/products/src/products/Products.js
+++ react/products/src/products/Products.js
@@ -1,6 +1,18 @@
 import './Products.css';
+import { useEffect, useState } from 'react';
 
 export default function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.shipap.co.il/products", {
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data);
+        });
+    }, []);
 
     return <p>Products works</p>
 }
