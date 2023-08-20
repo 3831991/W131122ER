1f5296df594bc5805b2db60287c4f46fab2d81d3
Your Name <you@example.com>
2023-08-20 19:34:41 +0300

    added

--- react/news-managment/src/authorization/Login.js
+++ react/news-managment/src/authorization/Login.js
@@ -1,12 +1,38 @@
-export default function Login() {
+import { useState } from "react"
 
+export default function Login() {
+    const [formData, setFormData] = useState({
+        userName: '',
+        password: '',
+    });
+
+    const handelInput = ev => {
+        const { name, value } = ev.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
 
     return (
         <>
             <div className="Login smallFrame">
                 <h2>התחברות</h2>
 
-                
+                <form>
+                    <label>
+                        שם משתמש:
+                        <input type="text" name="userName" value={formData.userName} onChange={handelInput} />
+                    </label>
+
+                    <label>
+                        סיסמה:
+                        <input type="password" name="password" value={formData.password} onChange={handelInput} />
+                    </label>
+
+                    <button>התחבר</button>
+                </form>
             </div>
         </>
     )
