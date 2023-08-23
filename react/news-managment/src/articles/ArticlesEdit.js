3b9eea91e4e7eba1d8cd5d6bce11e3ab0102ac24
Your Name <you@example.com>
2023-08-23 18:44:39 +0300

    added

--- react/news-managment/src/articles/ArticlesEdit.js
+++ react/news-managment/src/articles/ArticlesEdit.js
@@ -1,6 +1,10 @@
+import { useParams } from 'react-router-dom';
 import './Articles.css';
 
 export default function ArticlesEdit() {
+    const { id } = useParams(); 
+
+    
 
     return (
         <p>עריכת כתבה</p>
