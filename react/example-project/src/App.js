62b015914f2c24ccff26ac40cebd921b4f203d92
Your Name <you@example.com>
2023-09-06 20:08:46 +0300

    added

--- react/example-project/src/App.js
+++ react/example-project/src/App.js
@@ -1,17 +1,22 @@
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import './App.css';
 import Router from './Router';
-import Navbar from './components/Navbar';
+import Navbar, { RoleTypes } from './components/Navbar';
+import Loader from './components/Loader';
+
+export const GeneralContext = createContext();
 
 function App() {
     const [user, setUser] = useState();
-    
+    const [loader, setLoader] = useState(true);
+    const [userRoleType, setUserRoleType] = useState(RoleTypes.none);
 
     return (
-        <div className="App">
+        <GeneralContext.Provider value={{ user, setUser, setLoader, userRoleType, setUserRoleType }}>
             <Navbar />
             <Router />
-        </div>
+            {loader && <Loader />}
+        </GeneralContext.Provider>
     );
 }
 
