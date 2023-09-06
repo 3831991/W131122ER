cdf79147667054986d01766ea68ce863af698660
Your Name <you@example.com>
2023-09-06 19:01:18 +0300

    added

--- react/example-project/src/App.js
+++ react/example-project/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Router from './Router';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router />
     </div>
   );
 }
