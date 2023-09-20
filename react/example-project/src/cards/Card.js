1033af6311d9c188dc0e65c32630403965ef8f08
Your Name <you@example.com>
2023-09-20 20:59:41 +0300

    added

---
+++ react/example-project/src/cards/Card.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export default function MyCard({ card }) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        {card.title[0]}
+                    </Avatar>
+                }
+                title={card.title}
+                subheader={card.createdTime}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={card.imgUrl}
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {card.subtitle}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
--- react/example-project/src/cards/Cards.js
+++ react/example-project/src/cards/Cards.js
@@ -1,7 +1,20 @@
+import MyCard from "./Card";
+
 export default function Cards() {
+  const example = {
+    id: 1,
+    title: 'נגריית ים המלח',
+    createdTime: '2023-09-20 20:53:00',
+    imgUrl: 'https://www.היערהשחורגרמניה.com/wp-content/uploads/2016/03/chala-for-shabbat-3-1321701.jpg',
+    subtitle: 'חלות טעימות מאוד',
+    phone: '053-8989456456',
+    email: 'abcdef@gmail.com',
+  };
+
   return (
     <div>
         <h3 className="tempTitle">צפייה בכל הכרטיסים (דף ראשי)</h3>
+        <MyCard card={example} />
     </div>
   )
 }
