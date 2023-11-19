4aee4df6435e6af7f762571fed5106224e2f50a3
Your Name <you@example.com>
2023-11-19 20:09:04 +0200

    cors

--- nodejs/products/backend/index.js
+++ nodejs/products/backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 require('./sqlConnection');
 
 const app = express();
+
+app.use(cors({
+    origin: true,
+    methods: 'GET,PUT,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+}));
 
 app.listen(4000, () => {
     console.log('listening on port 4000');
