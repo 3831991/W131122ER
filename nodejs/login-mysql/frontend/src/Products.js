ffa08300b1d597d6cd4a412c1e8ffce83bac8e7e
Your Name <you@example.com>
2023-11-29 20:36:50 +0200

    added

---
+++ nodejs/login-mysql/frontend/src/Products.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { Link, useNavigate } from 'react-router-dom';
+import moment from 'moment';
+
+export default function Products() {
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch("http://localhost:4000/products")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+        .catch(err => console.error(err));
+    }, []);
+
+    const remove = (id) => {
+        if (!window.confirm("Are you sure you want to delete this product?")) {
+            return;
+        }
+
+        fetch(`http://localhost:4000/products/${id}`, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            const newData = products.filter(x => x.id !== id);
+            setProducts(newData);
+        });
+    }
+
+    return (
+        <div>
+            <div className='btnFrame'>
+                <button className='returnLink' onClick={() => navigate('/product/new')}>Add product</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Created Time</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Discount</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((a, i) =>
+                            <tr key={a.id}>
+                                <td>{i + 1}</td>
+                                <td>{moment(a.createdTime).format("D/M/Y")}</td>
+                                <td>{a.name}</td>
+                                <td>{a.price}</td>
+                                <td>{a.discount}</td>
+                                <td>
+                                    <Link to={`/product/${a.id}`}>
+                                        <button className="green"><AiFillEdit /></button>
+                                    </Link>
+                                    
+                                    <button className="red" onClick={() => remove(a.id)}><AiFillDelete /></button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- nodejs/login-mysql/frontend/src/Router.js
+++ nodejs/login-mysql/frontend/src/Router.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Products from './Products';
 
 export function Router() {
     return (
         <Routes>
+            <Route path="/" element={<Products />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
         </Routes>
